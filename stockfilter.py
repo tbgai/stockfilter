d66325e157d1ec57b239727b36c6703e7af0c8e3
James <jingzl@hotmail.com>
2019-08-10 23:05:33 +0800

    config

--- stockfilter.py
+++ stockfilter.py
@@ -8,12 +8,23 @@ Created on Sat Aug  3 16:47:39 2019
 # stockfilter.py
 
 
+# 读取股票参数基准配置
+def parseConfig():
+    pass
 
+# 读取待过滤的股票数据
+def parseStockData():
+    pass
 
+# 过滤比较
+def stockFilter():
+    pass
 
 
 def main():
-   pass 
+    parseConfig()
+    parseStockData()
+    stockFilter()
 
 
 main()
