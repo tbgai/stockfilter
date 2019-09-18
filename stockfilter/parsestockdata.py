cdebd47e89c95fcdd0b6d40adf906d4f24895c49
James <jingzl@hotmail.com>
2019-09-18 18:50:50 +0800

    init tushare pro and test data interface

--- stockfilter/parsestockdata.py
+++ stockfilter/parsestockdata.py
@@ -8,15 +8,36 @@ parse stock data from tushare interface
 """
 import tushare as ts
 
+TUSHARE_TOKEN = '8af63d3d7f7ec3bc0e511a85b13d0cf01c57dd310bc1f8ed4b902ad3'
+
 class ParseStockData( object ):
     
     def __init__( self ):
         pass
     
     def getStockData( self ):
-        # tushare的股票数据获取
-        print(ts.get_hist_data('600848',start='2019-08-29',end='2019-08-30'))
-        #print("parse stock data")
+        
+        # 获取所有的股票列表信息
+        pro = ts.pro_api( token=TUSHARE_TOKEN )
+        # 查询当前所有正常上市交易的股票列表
+        data = pro.query( 'stock_basic', exchange='', list_status='L', 
+                         fields='ts_code, symbol, name')
+        #print( data )
+        #print( data.ix[0:3] )
+        #print( data.columns )
+        #print( data.ix[ 0:3, 1 ] )
+        
+        dl = data.ix[ 0:3, 1 ].values
+        print( dl[1] )
+        
+        
+        df = pro.query( 'daily', ts_code='000001.SZ', start_date='20190818',
+                       end_date='20190918', fields='trade_date,close')
+        #print( df )
+        dl2 = df.ix[:,1].values
+        print( dl2 )
+        print( dl2[2] )
+        
 
 
 
