4ccc6942e6fb5bd12b31cdbcad9be4c3bf63efc2
James <jingzl@hotmail.com>
2019-09-19 13:41:27 +0800

    main函数流程调整完毕

--- stockfilter.py
+++ stockfilter.py
@@ -5,26 +5,38 @@ Created on Sat Aug  3 16:47:39 2019
 @author: jingzl
 """
 # stockfilter.py
+import os
+from stockfilter import cons as ct
+from stockfilter.configparser import ConfigParser
 from stockfilter.filter import StockFilter
 
 
-def main():
-
-    sfilter = StockFilter()
-    sfilter.filter()
+if __name__ == '__main__':
     
-    #factorls = []
-    #factorls = parseConfig( CONFIG_FNAME )
-    #print( factorls )
-    #np.savetxt( FACTOR_FNAME, factorls, fmt='%.6f' )
-    #print( factorls )
-    #sls = stockFilter( STOCK_FNAME, factorls )
-    #print( sls )
+    config_file = os.path.dirname( os.path.realpath(__file__)) + ct.BASESTOCK_FNAME
+    #print( config_file )
+    config = ConfigParser()
+    if config.parse( config_file ):
+        
+        basestock = config.getBaseStock()
+        second_derivative = config.getSecondDerivative()
+        output_pic = config.getOutputPic()
+        result_days = config.getResultDays()
+        print("配置如下：")
+        print("基准股票数据：" + str(basestock) )
+        print("二阶导：" + str(second_derivative) )
+        print("输出图片：" + str(output_pic) )
+        print("输出股票天数：" + str(result_days) )
+        
+        res = input( "请确认配置是否正确（Y/N）： " )
+        if res[0] in [ 'Y', 'y' ]:
+            sfilter = StockFilter( basestock, second_derivative, output_pic, result_days )
+            sfilter.filter()
+        elif res[0] in [ 'N', 'n' ]:
+            print( "程序退出..." )
+        else:
+            print( "输入错误！程序退出..." )
 
-main()
-
-
-
-
-
-
+    else:
+        print( "解析配置文件失败，请检查配置文件！" )
+    
